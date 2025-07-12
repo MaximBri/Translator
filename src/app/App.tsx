25e0b9d3c4150f3b122fad93fe30a05282fbf2c7
Maxim <MaximK38136@yandex.ru>
2025-07-12 23:25:40 +0400

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,17 @@
 import { HomePage } from '@/pages/home'
 import { ErrorBoundary } from './providers'
+import { useTheme } from '@/shared/hooks/useTheme'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
+  useTheme()
   return (
     <ErrorBoundary>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   )
 }
