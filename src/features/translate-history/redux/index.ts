8af808207c3bb88c083fcd46a24e8a87a35525c2
sentares <aydararzybekov2002@mail.ru>
2025-07-13 04:26:35 +0600

    fix: update exporting in index.ts

--- src/features/translate-history/redux/index.ts
+++ src/features/translate-history/redux/index.ts
@@ -1,3 +1,4 @@
-import { historySlice } from './slice'
+import { historySlice, selectHistoryItems } from './slice'
 
 export const historyReducer = historySlice.reducer
+export { selectHistoryItems }
