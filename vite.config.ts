daacc459aba525f70e55aa4ca7110f7809e7d47d
Maxim <MaximK38136@yandex.ru>
2025-07-13 15:29:37 +0400

    chore(configs): some changes with ts, vite and eslint configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import manifest from './public/manifest.json'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    crx({
+      manifest,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
